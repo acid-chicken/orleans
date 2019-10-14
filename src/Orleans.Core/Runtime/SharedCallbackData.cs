ee85a808a909a3788e6993ed1033239752129a68
Restyled.io <commits@restyled.io>
2019-10-14 18:01:23 +0000

    Restyled by astyle

--- src/Orleans.Core/Runtime/SharedCallbackData.cs
+++ src/Orleans.Core/Runtime/SharedCallbackData.cs
@@ -6,90 +6,92 @@ using Orleans.Configuration;
 
 namespace Orleans.Runtime
 {
-    internal class SharedCallbackData
+internal class SharedCallbackData
+{
+    public readonly Action<Message> Unregister;
+    public readonly ILogger Logger;
+    public readonly MessagingOptions MessagingOptions;
+    private TimeSpan responseTimeout;
+    public long ResponseTimeoutStopwatchTicks;
+
+    public SharedCallbackData(
+        Action<Message> unregister,
+        ILogger logger,
+        MessagingOptions messagingOptions,
+        ApplicationRequestsStatisticsGroup requestStatistics,
+        TimeSpan responseTimeout)
     {
-        public readonly Action<Message> Unregister;
-        public readonly ILogger Logger;
-        public readonly MessagingOptions MessagingOptions;
-        private TimeSpan responseTimeout;
-        public long ResponseTimeoutStopwatchTicks;
+        RequestStatistics = requestStatistics;
+        this.Unregister = unregister;
+        this.Logger = logger;
+        this.MessagingOptions = messagingOptions;
+        this.ResponseTimeout = responseTimeout;
+    }
 
-        public SharedCallbackData(
-            Action<Message> unregister,
-            ILogger logger,
-            MessagingOptions messagingOptions,
-            ApplicationRequestsStatisticsGroup requestStatistics,
-            TimeSpan responseTimeout)
+    public ApplicationRequestsStatisticsGroup RequestStatistics {
+        get;
+    }
+
+    public TimeSpan ResponseTimeout
+    {
+        get => this.responseTimeout;
+        set
         {
-            RequestStatistics = requestStatistics;
-            this.Unregister = unregister;
-            this.Logger = logger;
-            this.MessagingOptions = messagingOptions;
-            this.ResponseTimeout = responseTimeout;
-        }
-
-        public ApplicationRequestsStatisticsGroup RequestStatistics { get; }
-
-        public TimeSpan ResponseTimeout
-        {
-            get => this.responseTimeout;
-            set
-            {
-                this.responseTimeout = value;
-                this.ResponseTimeoutStopwatchTicks = (long)(value.TotalSeconds * Stopwatch.Frequency);
-            }
-        }
-
-        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
-        public void ResponseCallback(Message message, TaskCompletionSource<object> context)
-        {
-            Response response;
-            if (message.Result != Message.ResponseTypes.Rejection)
-            {
-                try
-                {
-                    response = (Response)message.BodyObject;
-                }
-                catch (Exception exc)
-                {
-                    //  catch the Deserialize exception and break the promise with it.
-                    response = Response.ExceptionResponse(exc);
-                }
-            }
-            else
-            {
-                Exception rejection;
-                switch (message.RejectionType)
-                {
-                    case Message.RejectionTypes.GatewayTooBusy:
-                        rejection = new GatewayTooBusyException();
-                        break;
-                    case Message.RejectionTypes.DuplicateRequest:
-                        return; // Ignore duplicates
-
-                    default:
-                        rejection = message.BodyObject as Exception;
-                        if (rejection == null)
-                        {
-                            if (string.IsNullOrEmpty(message.RejectionInfo))
-                            {
-                                message.RejectionInfo = "Unable to send request - no rejection info available";
-                            }
-                            rejection = new OrleansMessageRejectionException(message.RejectionInfo);
-                        }
-                        break;
-                }
-                response = Response.ExceptionResponse(rejection);
-            }
-
-            if (!response.ExceptionFlag)
-            {
-                context.TrySetResult(response.Data);
-            }
-            else
-            {
-                context.TrySetException(response.Exception);
-            }
+            this.responseTimeout = value;
+            this.ResponseTimeoutStopwatchTicks = (long)(value.TotalSeconds * Stopwatch.Frequency);
         }
     }
+
+    [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes")]
+    public void ResponseCallback(Message message, TaskCompletionSource<object> context)
+    {
+        Response response;
+        if (message.Result != Message.ResponseTypes.Rejection)
+        {
+            try
+            {
+                response = (Response)message.BodyObject;
+            }
+            catch (Exception exc)
+            {
+                //  catch the Deserialize exception and break the promise with it.
+                response = Response.ExceptionResponse(exc);
+            }
+        }
+        else
+        {
+            Exception rejection;
+            switch (message.RejectionType)
+            {
+            case Message.RejectionTypes.GatewayTooBusy:
+                rejection = new GatewayTooBusyException();
+                break;
+            case Message.RejectionTypes.DuplicateRequest:
+                return; // Ignore duplicates
+
+            default:
+                rejection = message.BodyObject as Exception;
+                if (rejection == null)
+                {
+                    if (string.IsNullOrEmpty(message.RejectionInfo))
+                    {
+                        message.RejectionInfo = "Unable to send request - no rejection info available";
+                    }
+                    rejection = new OrleansMessageRejectionException(message.RejectionInfo);
+                }
+                break;
+            }
+            response = Response.ExceptionResponse(rejection);
+        }
+
+        if (!response.ExceptionFlag)
+        {
+            context.TrySetResult(response.Data);
+        }
+        else
+        {
+            context.TrySetException(response.Exception);
+        }
+    }
+}
 }
