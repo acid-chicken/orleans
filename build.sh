d31ffaa319a0daefb48e14d771bcff4b4b9b8377
Restyled.io <commits@restyled.io>
2019-10-19 21:01:23 +0000

    Restyled by shfmt

--- build.sh
+++ build.sh
@@ -10,11 +10,11 @@ set -e
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
   SOURCE="$(readlink "$SOURCE")"
   [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 
 DOCKER_IMAGENAME=mcr.microsoft.com/dotnet/core/sdk:3.0
 
@@ -41,7 +41,7 @@ echo "Running command: $BUILD_COMMAND"
 echo "Using code from: $DOCKER_HOST_SHARE_DIR"
 
 docker run -t --rm --sig-proxy=true \
-    --name docker-orleansbuild \
-    -v "$DOCKER_HOST_SHARE_DIR":/opt/code \
-    -w /opt/code \
-    "$DOCKER_IMAGENAME" "$BUILD_COMMAND" "$@"
+  --name docker-orleansbuild \
+  -v "$DOCKER_HOST_SHARE_DIR":/opt/code \
+  -w /opt/code \
+  "$DOCKER_IMAGENAME" "$BUILD_COMMAND" "$@"
