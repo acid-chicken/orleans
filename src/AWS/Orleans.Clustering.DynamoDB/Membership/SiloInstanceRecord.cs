66b99db5dacf5f03959d27813e971add7e0bb416
Restyled.io <commits@restyled.io>
2019-11-25 18:04:23 +0000

    Restyled by clang-format

--- src/AWS/Orleans.Clustering.DynamoDB/Membership/SiloInstanceRecord.cs
+++ src/AWS/Orleans.Clustering.DynamoDB/Membership/SiloInstanceRecord.cs
@@ -4,10 +4,8 @@ using System.Collections.Generic;
 using System.Net;
 using System.Text;
 
-namespace Orleans.Runtime.MembershipService
-{
-internal class SiloInstanceRecord
-{
+namespace Orleans.Runtime.MembershipService {
+  internal class SiloInstanceRecord {
     public const string DEPLOYMENT_ID_PROPERTY_NAME = "DeploymentId";
     public const string SILO_IDENTITY_PROPERTY_NAME = "SiloIdentity";
     public const string ETAG_PROPERTY_NAME = "ETag";
@@ -24,229 +22,234 @@ internal class SiloInstanceRecord
     public const string START_TIME_PROPERTY_NAME = "StartTime";
     public const string I_AM_ALIVE_TIME_PROPERTY_NAME = "IAmAliveTime";
     internal const char Seperator = '-';
-    internal const string TABLE_VERSION_ROW = "VersionRow"; // Range key for version row.
+    internal const string TABLE_VERSION_ROW =
+        "VersionRow"; // Range key for version row.
     public const string MEMBERSHIP_VERSION_PROPERTY_NAME = "MembershipVersion";
 
     public string DeploymentId {
-        get;
-        set;
+      get;
+      set;
     }
     public string SiloIdentity {
-        get;
-        set;
+      get;
+      set;
     }
     public string Address {
-        get;
-        set;
+      get;
+      set;
     }
     public int Port {
-        get;
-        set;
+      get;
+      set;
     }
     public int Generation {
-        get;
-        set;
+      get;
+      set;
     }
     public string HostName {
-        get;
-        set;
+      get;
+      set;
     }
     public int Status {
-        get;
-        set;
+      get;
+      set;
     }
     public int ProxyPort {
-        get;
-        set;
+      get;
+      set;
     }
     public string SiloName {
-        get;
-        set;
+      get;
+      set;
     }
     public string SuspectingSilos {
-        get;
-        set;
+      get;
+      set;
     }
     public string SuspectingTimes {
-        get;
-        set;
+      get;
+      set;
     }
     public string StartTime {
-        get;
-        set;
+      get;
+      set;
     }
     public string IAmAliveTime {
-        get;
-        set;
+      get;
+      set;
     }
     public int ETag {
-        get;
-        set;
+      get;
+      set;
     }
 
     public int MembershipVersion {
-        get;
-        set;
+      get;
+      set;
     }
 
-    public SiloInstanceRecord() { }
+    public SiloInstanceRecord() {}
 
-    public SiloInstanceRecord(Dictionary<string, AttributeValue> fields)
-    {
-        if (fields.ContainsKey(DEPLOYMENT_ID_PROPERTY_NAME))
-            DeploymentId = fields[DEPLOYMENT_ID_PROPERTY_NAME].S;
+    public SiloInstanceRecord(Dictionary<string, AttributeValue>fields) {
+      if (fields.ContainsKey(DEPLOYMENT_ID_PROPERTY_NAME))
+        DeploymentId = fields[DEPLOYMENT_ID_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(SILO_IDENTITY_PROPERTY_NAME))
-            SiloIdentity = fields[SILO_IDENTITY_PROPERTY_NAME].S;
+      if (fields.ContainsKey(SILO_IDENTITY_PROPERTY_NAME))
+        SiloIdentity = fields[SILO_IDENTITY_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(ADDRESS_PROPERTY_NAME))
-            Address = fields[ADDRESS_PROPERTY_NAME].S;
+      if (fields.ContainsKey(ADDRESS_PROPERTY_NAME))
+        Address = fields[ADDRESS_PROPERTY_NAME].S;
 
-        int port;
-        if (fields.ContainsKey(PORT_PROPERTY_NAME) &&
-                int.TryParse(fields[PORT_PROPERTY_NAME].N, out port))
-            Port = port;
+      int port;
+      if (fields.ContainsKey(PORT_PROPERTY_NAME) &&
+          int.TryParse(fields[PORT_PROPERTY_NAME].N, out port))
+        Port = port;
 
-        int generation;
-        if (fields.ContainsKey(GENERATION_PROPERTY_NAME) &&
-                int.TryParse(fields[GENERATION_PROPERTY_NAME].N, out generation))
-            Generation = generation;
+      int generation;
+      if (fields.ContainsKey(GENERATION_PROPERTY_NAME) &&
+          int.TryParse(fields[GENERATION_PROPERTY_NAME].N, out generation))
+        Generation = generation;
 
-        if (fields.ContainsKey(HOSTNAME_PROPERTY_NAME))
-            HostName = fields[HOSTNAME_PROPERTY_NAME].S;
+      if (fields.ContainsKey(HOSTNAME_PROPERTY_NAME))
+        HostName = fields[HOSTNAME_PROPERTY_NAME].S;
 
-        int status;
-        if (fields.ContainsKey(STATUS_PROPERTY_NAME) &&
-                int.TryParse(fields[STATUS_PROPERTY_NAME].N, out status))
-            Status = status;
+      int status;
+      if (fields.ContainsKey(STATUS_PROPERTY_NAME) &&
+          int.TryParse(fields[STATUS_PROPERTY_NAME].N, out status))
+        Status = status;
 
-        int proxyPort;
-        if (fields.ContainsKey(PROXY_PORT_PROPERTY_NAME) &&
-                int.TryParse(fields[PROXY_PORT_PROPERTY_NAME].N, out proxyPort))
-            ProxyPort = proxyPort;
+      int proxyPort;
+      if (fields.ContainsKey(PROXY_PORT_PROPERTY_NAME) &&
+          int.TryParse(fields[PROXY_PORT_PROPERTY_NAME].N, out proxyPort))
+        ProxyPort = proxyPort;
 
-        if (fields.ContainsKey(SILO_NAME_PROPERTY_NAME))
-            SiloName = fields[SILO_NAME_PROPERTY_NAME].S;
+      if (fields.ContainsKey(SILO_NAME_PROPERTY_NAME))
+        SiloName = fields[SILO_NAME_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(SUSPECTING_SILOS_PROPERTY_NAME))
-            SuspectingSilos = fields[SUSPECTING_SILOS_PROPERTY_NAME].S;
+      if (fields.ContainsKey(SUSPECTING_SILOS_PROPERTY_NAME))
+        SuspectingSilos = fields[SUSPECTING_SILOS_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(SUSPECTING_TIMES_PROPERTY_NAME))
-            SuspectingTimes = fields[SUSPECTING_TIMES_PROPERTY_NAME].S;
+      if (fields.ContainsKey(SUSPECTING_TIMES_PROPERTY_NAME))
+        SuspectingTimes = fields[SUSPECTING_TIMES_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(START_TIME_PROPERTY_NAME))
-            StartTime = fields[START_TIME_PROPERTY_NAME].S;
+      if (fields.ContainsKey(START_TIME_PROPERTY_NAME))
+        StartTime = fields[START_TIME_PROPERTY_NAME].S;
 
-        if (fields.ContainsKey(I_AM_ALIVE_TIME_PROPERTY_NAME))
-            IAmAliveTime = fields[I_AM_ALIVE_TIME_PROPERTY_NAME].S;
+      if (fields.ContainsKey(I_AM_ALIVE_TIME_PROPERTY_NAME))
+        IAmAliveTime = fields[I_AM_ALIVE_TIME_PROPERTY_NAME].S;
 
-        int etag;
-        if (fields.ContainsKey(ETAG_PROPERTY_NAME) &&
-                int.TryParse(fields[ETAG_PROPERTY_NAME].N, out etag))
-            ETag = etag;
+      int etag;
+      if (fields.ContainsKey(ETAG_PROPERTY_NAME) &&
+          int.TryParse(fields[ETAG_PROPERTY_NAME].N, out etag))
+        ETag = etag;
 
-        if (fields.ContainsKey(MEMBERSHIP_VERSION_PROPERTY_NAME) &&
-                int.TryParse(fields[MEMBERSHIP_VERSION_PROPERTY_NAME].N, out int version))
-            MembershipVersion = version;
+      if (fields.ContainsKey(MEMBERSHIP_VERSION_PROPERTY_NAME) &&
+          int.TryParse(fields[MEMBERSHIP_VERSION_PROPERTY_NAME].N,
+                       out int version))
+        MembershipVersion = version;
     }
 
-    internal static SiloAddress UnpackRowKey(string rowKey)
-    {
-        try
-        {
-            int idx1 = rowKey.IndexOf(Seperator);
-            int idx2 = rowKey.LastIndexOf(Seperator);
-            var addressStr = rowKey.Substring(0, idx1);
-            var portStr = rowKey.Substring(idx1 + 1, idx2 - idx1 - 1);
-            var genStr = rowKey.Substring(idx2 + 1);
-            IPAddress address = IPAddress.Parse(addressStr);
-            int port = Int32.Parse(portStr);
-            int generation = Int32.Parse(genStr);
-            return SiloAddress.New(new IPEndPoint(address, port), generation);
-        }
-        catch (Exception exc)
-        {
-            throw new AggregateException("Error from UnpackRowKey", exc);
-        }
+    internal static SiloAddress UnpackRowKey(string rowKey) {
+      try {
+        int idx1 = rowKey.IndexOf(Seperator);
+        int idx2 = rowKey.LastIndexOf(Seperator);
+        var addressStr = rowKey.Substring(0, idx1);
+        var portStr = rowKey.Substring(idx1 + 1, idx2 - idx1 - 1);
+        var genStr = rowKey.Substring(idx2 + 1);
+        IPAddress address = IPAddress.Parse(addressStr);
+        int port = Int32.Parse(portStr);
+        int generation = Int32.Parse(genStr);
+        return SiloAddress.New(new IPEndPoint(address, port), generation);
+      } catch (Exception exc) {
+        throw new AggregateException("Error from UnpackRowKey", exc);
+      }
     }
 
-    public override string ToString()
-    {
-        var sb = new StringBuilder();
-        sb.Append("OrleansSilo [");
-        sb.Append(" Deployment=").Append(DeploymentId);
-        sb.Append(" LocalEndpoint=").Append(Address);
-        sb.Append(" LocalPort=").Append(Port);
-        sb.Append(" Generation=").Append(Generation);
+    public override string ToString() {
+      var sb = new StringBuilder();
+      sb.Append("OrleansSilo [");
+      sb.Append(" Deployment=").Append(DeploymentId);
+      sb.Append(" LocalEndpoint=").Append(Address);
+      sb.Append(" LocalPort=").Append(Port);
+      sb.Append(" Generation=").Append(Generation);
 
-        sb.Append(" Host=").Append(HostName);
-        sb.Append(" Status=").Append(Status);
-        sb.Append(" ProxyPort=").Append(ProxyPort);
+      sb.Append(" Host=").Append(HostName);
+      sb.Append(" Status=").Append(Status);
+      sb.Append(" ProxyPort=").Append(ProxyPort);
 
-        sb.Append(" SiloName=").Append(SiloName);
+      sb.Append(" SiloName=").Append(SiloName);
 
-        if (!string.IsNullOrEmpty(SuspectingSilos)) sb.Append(" SuspectingSilos=").Append(SuspectingSilos);
-        if (!string.IsNullOrEmpty(SuspectingTimes)) sb.Append(" SuspectingTimes=").Append(SuspectingTimes);
-        sb.Append(" StartTime=").Append(StartTime);
-        sb.Append(" IAmAliveTime=").Append(IAmAliveTime);
-        sb.Append("]");
-        return sb.ToString();
+      if (!string.IsNullOrEmpty(SuspectingSilos))
+        sb.Append(" SuspectingSilos=").Append(SuspectingSilos);
+      if (!string.IsNullOrEmpty(SuspectingTimes))
+        sb.Append(" SuspectingTimes=").Append(SuspectingTimes);
+      sb.Append(" StartTime=").Append(StartTime);
+      sb.Append(" IAmAliveTime=").Append(IAmAliveTime);
+      sb.Append("]");
+      return sb.ToString();
     }
 
-    public static string ConstructSiloIdentity(SiloAddress silo)
-    {
-        return string.Format("{0}-{1}-{2}", silo.Endpoint.Address, silo.Endpoint.Port, silo.Generation);
+    public static string ConstructSiloIdentity(SiloAddress silo) {
+      return string.Format("{0}-{1}-{2}", silo.Endpoint.Address,
+                           silo.Endpoint.Port, silo.Generation);
     }
 
-    public Dictionary<string, AttributeValue> GetKeys()
-    {
-        var keys = new Dictionary<string, AttributeValue>();
-        keys.Add(DEPLOYMENT_ID_PROPERTY_NAME, new AttributeValue(DeploymentId));
-        keys.Add(SILO_IDENTITY_PROPERTY_NAME, new AttributeValue(SiloIdentity));
-        return keys;
+    public Dictionary<string, AttributeValue>GetKeys() {
+      var keys = new Dictionary<string, AttributeValue>();
+      keys.Add(DEPLOYMENT_ID_PROPERTY_NAME, new AttributeValue(DeploymentId));
+      keys.Add(SILO_IDENTITY_PROPERTY_NAME, new AttributeValue(SiloIdentity));
+      return keys;
     }
 
-    public Dictionary<string, AttributeValue> GetFields(bool includeKeys = false)
-    {
-        var fields = new Dictionary<string, AttributeValue>();
+    public Dictionary<string, AttributeValue>
+    GetFields(bool includeKeys = false) {
+      var fields = new Dictionary<string, AttributeValue>();
 
-        if (includeKeys)
-        {
-            fields.Add(DEPLOYMENT_ID_PROPERTY_NAME, new AttributeValue(DeploymentId));
-            fields.Add(SILO_IDENTITY_PROPERTY_NAME, new AttributeValue(SiloIdentity));
-        }
+      if (includeKeys) {
+        fields.Add(DEPLOYMENT_ID_PROPERTY_NAME,
+                   new AttributeValue(DeploymentId));
+        fields.Add(SILO_IDENTITY_PROPERTY_NAME,
+                   new AttributeValue(SiloIdentity));
+      }
 
-        if (!string.IsNullOrWhiteSpace(Address))
-            fields.Add(ADDRESS_PROPERTY_NAME, new AttributeValue(Address));
+      if (!string.IsNullOrWhiteSpace(Address))
+        fields.Add(ADDRESS_PROPERTY_NAME, new AttributeValue(Address));
 
-        fields.Add(PORT_PROPERTY_NAME, new AttributeValue { N = Port.ToString() });
-        fields.Add(GENERATION_PROPERTY_NAME, new AttributeValue { N = Generation.ToString() });
+      fields.Add(PORT_PROPERTY_NAME, new AttributeValue{N = Port.ToString()});
+      fields.Add(GENERATION_PROPERTY_NAME,
+                 new AttributeValue{N = Generation.ToString()});
 
-        if (!string.IsNullOrWhiteSpace(HostName))
-            fields.Add(HOSTNAME_PROPERTY_NAME, new AttributeValue(HostName));
+      if (!string.IsNullOrWhiteSpace(HostName))
+        fields.Add(HOSTNAME_PROPERTY_NAME, new AttributeValue(HostName));
 
-        fields.Add(STATUS_PROPERTY_NAME, new AttributeValue { N = Status.ToString() });
-        fields.Add(PROXY_PORT_PROPERTY_NAME, new AttributeValue { N = ProxyPort.ToString() });
+      fields.Add(STATUS_PROPERTY_NAME,
+                 new AttributeValue{N = Status.ToString()});
+      fields.Add(PROXY_PORT_PROPERTY_NAME,
+                 new AttributeValue{N = ProxyPort.ToString()});
 
-        if (!string.IsNullOrWhiteSpace(SiloName))
-            fields.Add(SILO_NAME_PROPERTY_NAME, new AttributeValue(SiloName));
+      if (!string.IsNullOrWhiteSpace(SiloName))
+        fields.Add(SILO_NAME_PROPERTY_NAME, new AttributeValue(SiloName));
 
-        if (!string.IsNullOrWhiteSpace(SuspectingSilos))
-            fields.Add(SUSPECTING_SILOS_PROPERTY_NAME, new AttributeValue(SuspectingSilos));
+      if (!string.IsNullOrWhiteSpace(SuspectingSilos))
+        fields.Add(SUSPECTING_SILOS_PROPERTY_NAME,
+                   new AttributeValue(SuspectingSilos));
 
-        if (!string.IsNullOrWhiteSpace(SuspectingTimes))
-            fields.Add(SUSPECTING_TIMES_PROPERTY_NAME, new AttributeValue(SuspectingTimes));
+      if (!string.IsNullOrWhiteSpace(SuspectingTimes))
+        fields.Add(SUSPECTING_TIMES_PROPERTY_NAME,
+                   new AttributeValue(SuspectingTimes));
 
-        if (!string.IsNullOrWhiteSpace(StartTime))
-            fields.Add(START_TIME_PROPERTY_NAME, new AttributeValue(StartTime));
+      if (!string.IsNullOrWhiteSpace(StartTime))
+        fields.Add(START_TIME_PROPERTY_NAME, new AttributeValue(StartTime));
 
-        if (!string.IsNullOrWhiteSpace(IAmAliveTime))
-            fields.Add(I_AM_ALIVE_TIME_PROPERTY_NAME, new AttributeValue(IAmAliveTime));
+      if (!string.IsNullOrWhiteSpace(IAmAliveTime))
+        fields.Add(I_AM_ALIVE_TIME_PROPERTY_NAME,
+                   new AttributeValue(IAmAliveTime));
 
-        fields.Add(MEMBERSHIP_VERSION_PROPERTY_NAME, new AttributeValue { N = MembershipVersion.ToString() });
+      fields.Add(MEMBERSHIP_VERSION_PROPERTY_NAME,
+                 new AttributeValue{N = MembershipVersion.ToString()});
 
-        fields.Add(ETAG_PROPERTY_NAME, new AttributeValue { N = ETag.ToString() });
-        return fields;
+      fields.Add(ETAG_PROPERTY_NAME, new AttributeValue{N = ETag.ToString()});
+      return fields;
     }
-}
+  }
 }
--- test/Extensions/AWSUtils.Tests/MembershipTests/DynamoDBMembershipTableTest.cs
+++ test/Extensions/AWSUtils.Tests/MembershipTests/DynamoDBMembershipTableTest.cs
@@ -11,94 +11,99 @@ using UnitTests;
 using UnitTests.MembershipTests;
 using Xunit;
 
-namespace AWSUtils.Tests.MembershipTests
-{
-/// <summary>
-/// Tests for operation of Orleans Membership Table using AWS DynamoDB - Requires access to external DynamoDB storage
-/// </summary>
-[TestCategory("Membership"), TestCategory("AWS"), TestCategory("DynamoDb")]
-public class DynamoDBMembershipTableTest : MembershipTableTestsBase, IClassFixture<DynamoDBStorageTestsFixture>
-{
-    public DynamoDBMembershipTableTest(ConnectionStringFixture fixture, TestEnvironmentFixture environment) : base(fixture, environment, CreateFilters())
-    {
+namespace AWSUtils.Tests.MembershipTests {
+  /// <summary>
+  /// Tests for operation of Orleans Membership Table using AWS DynamoDB -
+  /// Requires access to external DynamoDB storage
+  /// </summary>
+  [TestCategory("Membership"), TestCategory("AWS"), TestCategory("DynamoDb")]
+  public class DynamoDBMembershipTableTest
+      : MembershipTableTestsBase,
+        IClassFixture<DynamoDBStorageTestsFixture> {
+    public DynamoDBMembershipTableTest(ConnectionStringFixture fixture,
+                                       TestEnvironmentFixture environment)
+        : base(fixture, environment, CreateFilters()) {}
+
+    private static LoggerFilterOptions CreateFilters() {
+      var filters = new LoggerFilterOptions();
+      filters.AddFilter("DynamoDBDataManager", LogLevel.Trace);
+      filters.AddFilter("OrleansSiloInstanceManager", LogLevel.Trace);
+      filters.AddFilter("Storage", LogLevel.Trace);
+      return filters;
     }
 
-    private static LoggerFilterOptions CreateFilters()
-    {
-        var filters = new LoggerFilterOptions();
-        filters.AddFilter("DynamoDBDataManager", LogLevel.Trace);
-        filters.AddFilter("OrleansSiloInstanceManager", LogLevel.Trace);
-        filters.AddFilter("Storage", LogLevel.Trace);
-        return filters;
+    protected override IMembershipTable CreateMembershipTable(ILogger logger) {
+      if (!AWSTestConstants.IsDynamoDbAvailable)
+        throw new SkipException("Unable to connect to AWS DynamoDB simulator");
+      var options = new DynamoDBClusteringOptions();
+      DynamoDBMembershipHelper.ParseDataConnectionString(this.connectionString,
+                                                         options);
+      return new DynamoDBMembershipTable(
+          this.loggerFactory, Options.Create(options), this.clusterOptions);
     }
 
-    protected override IMembershipTable CreateMembershipTable(ILogger logger)
-    {
-        if (!AWSTestConstants.IsDynamoDbAvailable)
-            throw new SkipException("Unable to connect to AWS DynamoDB simulator");
-        var options = new DynamoDBClusteringOptions();
-        DynamoDBMembershipHelper.ParseDataConnectionString(this.connectionString, options);
-        return new DynamoDBMembershipTable(this.loggerFactory, Options.Create(options), this.clusterOptions);
+    protected override IGatewayListProvider
+    CreateGatewayListProvider(ILogger logger) {
+      var options = new DynamoDBGatewayOptions();
+      DynamoDBGatewayListProviderHelper.ParseDataConnectionString(
+          this.connectionString, options);
+      return new DynamoDBGatewayListProvider(
+          this.loggerFactory, Options.Create(options), this.clusterOptions,
+          this.gatewayOptions);
     }
 
-    protected override IGatewayListProvider CreateGatewayListProvider(ILogger logger)
-    {
-        var options = new DynamoDBGatewayOptions();
-        DynamoDBGatewayListProviderHelper.ParseDataConnectionString(this.connectionString, options);
-        return new DynamoDBGatewayListProvider(this.loggerFactory, Options.Create(options), this.clusterOptions, this.gatewayOptions);
-    }
-
-    protected override Task<string> GetConnectionString()
-    {
-        return Task.FromResult(AWSTestConstants.IsDynamoDbAvailable ? $"Service={AWSTestConstants.Service}" : null);
-    }
-
-    [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_GetGateways()
-    {
-        await MembershipTable_GetGateways();
-    }
-
-    [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_ReadAll_EmptyTable()
-    {
-        await MembershipTable_ReadAll_EmptyTable();
+    protected override Task<string>GetConnectionString() {
+      return Task.FromResult(AWSTestConstants.IsDynamoDbAvailable ?
+                             $"Service={AWSTestConstants.Service}"
+                             : null);
     }
 
     [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_InsertRow()
-    {
-        await MembershipTable_InsertRow();
+    public async Task
+    MembershipTable_DynamoDB_GetGateways() {
+      await MembershipTable_GetGateways();
     }
 
     [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_ReadRow_Insert_Read()
-    {
-        await MembershipTable_ReadRow_Insert_Read();
+    public async Task
+    MembershipTable_DynamoDB_ReadAll_EmptyTable() {
+      await MembershipTable_ReadAll_EmptyTable();
     }
 
     [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_ReadAll_Insert_ReadAll()
-    {
-        await MembershipTable_ReadAll_Insert_ReadAll();
+    public async Task
+    MembershipTable_DynamoDB_InsertRow() {
+      await MembershipTable_InsertRow();
     }
 
     [SkippableFact, TestCategory("Functional")]
-    public async Task MembershipTable_DynamoDB_UpdateRow()
-    {
-        await MembershipTable_UpdateRow();
+    public async Task
+    MembershipTable_DynamoDB_ReadRow_Insert_Read() {
+      await MembershipTable_ReadRow_Insert_Read();
+    }
+
+    [SkippableFact, TestCategory("Functional")]
+    public async Task
+    MembershipTable_DynamoDB_ReadAll_Insert_ReadAll() {
+      await MembershipTable_ReadAll_Insert_ReadAll();
+    }
+
+    [SkippableFact, TestCategory("Functional")]
+    public async Task
+    MembershipTable_DynamoDB_UpdateRow() {
+      await MembershipTable_UpdateRow();
     }
 
     [SkippableFact]
-    public async Task MembershipTable_DynamoDB_UpdateRowInParallel()
-    {
-        await MembershipTable_UpdateRowInParallel();
+    public async Task
+    MembershipTable_DynamoDB_UpdateRowInParallel() {
+      await MembershipTable_UpdateRowInParallel();
     }
 
     [SkippableFact]
-    public async Task MembershipTable_DynamoDB_UpdateIAmAlive()
-    {
-        await MembershipTable_UpdateIAmAlive();
+    public async Task
+    MembershipTable_DynamoDB_UpdateIAmAlive() {
+      await MembershipTable_UpdateIAmAlive();
     }
-}
+  }
 }
