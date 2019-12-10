330a496929e2cf5a62c498b5f93e1ea72a82a96b
Restyled.io <commits@restyled.io>
2019-12-10 18:04:24 +0000

    Restyled by clang-format

--- src/Orleans.Core/Networking/Shared/SocketConnectionFactory.cs
+++ src/Orleans.Core/Networking/Shared/SocketConnectionFactory.cs
@@ -10,81 +10,80 @@ using Microsoft.AspNetCore.Connections;
 using Microsoft.Extensions.Logging;
 using Orleans.Runtime;
 
-namespace Orleans.Networking.Shared
-{
-internal class SocketConnectionFactory : IConnectionFactory
-{
+namespace Orleans.Networking.Shared {
+  internal class SocketConnectionFactory : IConnectionFactory {
     private readonly SocketsTrace trace;
     private readonly SocketSchedulers schedulers;
-    private readonly MemoryPool<byte> memoryPool;
+    private readonly MemoryPool<byte>memoryPool;
 
-    public SocketConnectionFactory(ILoggerFactory loggerFactory, SocketSchedulers schedulers, SharedMemoryPool memoryPool)
-    {
-        var logger = loggerFactory.CreateLogger("Orleans.Sockets");
-        this.trace = new SocketsTrace(logger);
-        this.schedulers = schedulers;
-        this.memoryPool = memoryPool.Pool;
+    public SocketConnectionFactory(ILoggerFactory loggerFactory,
+                                   SocketSchedulers schedulers,
+                                   SharedMemoryPool memoryPool) {
+      var logger = loggerFactory.CreateLogger("Orleans.Sockets");
+      this.trace = new SocketsTrace(logger);
+      this.schedulers = schedulers;
+      this.memoryPool = memoryPool.Pool;
     }
 
-    public async ValueTask<ConnectionContext> ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken)
-    {
-        var socket = new Socket(endpoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp)
-        {
-            LingerState = new LingerOption(true, 0),
-            NoDelay = true
-        };
+    public async ValueTask<ConnectionContext>
+    ConnectAsync(EndPoint endpoint, CancellationToken cancellationToken) {
+      var socket = new Socket(endpoint.AddressFamily, SocketType.Stream,
+                              ProtocolType.Tcp){
+          LingerState = new LingerOption(true, 0), NoDelay = true};
 
-        socket.EnableFastPath();
-        var completion = new SingleUseSocketAsyncEventArgs
-        {
-            RemoteEndPoint = endpoint
-        };
+      socket.EnableFastPath();
+      var completion =
+          new SingleUseSocketAsyncEventArgs{RemoteEndPoint = endpoint};
 
-        if (!socket.ConnectAsync(completion))
-        {
-            completion.Complete();
-        }
+      if (!socket.ConnectAsync(completion)) {
+        completion.Complete();
+      }
 
-        await completion;
+      await completion;
 
-        if (completion.SocketError != SocketError.Success)
-        {
-            throw new SocketConnectionException($"Unable to connect to {endpoint}. Error: {completion.SocketError}");
-        }
+      if (completion.SocketError != SocketError.Success) {
+        throw new SocketConnectionException(
+            $"Unable to connect to {endpoint}. Error: {completion.SocketError}");
+      }
 
-        var scheduler = this.schedulers.GetScheduler();
-        var connection = new SocketConnection(socket, this.memoryPool, scheduler, this.trace);
-        connection.Start();
-        return connection;
+      var scheduler = this.schedulers.GetScheduler();
+      var connection =
+          new SocketConnection(socket, this.memoryPool, scheduler, this.trace);
+      connection.Start();
+      return connection;
     }
 
-    public class SingleUseSocketAsyncEventArgs : SocketAsyncEventArgs, ICriticalNotifyCompletion
-    {
-        private readonly TaskCompletionSource<SingleUseSocketAsyncEventArgs> completion
-            = new TaskCompletionSource<SingleUseSocketAsyncEventArgs>();
+    public class SingleUseSocketAsyncEventArgs : SocketAsyncEventArgs,
+                                                 ICriticalNotifyCompletion {
+      private readonly TaskCompletionSource<SingleUseSocketAsyncEventArgs>
+          completion =
+              new TaskCompletionSource<SingleUseSocketAsyncEventArgs>();
 
-        public TaskAwaiter<SingleUseSocketAsyncEventArgs> GetAwaiter() => this.completion.Task.GetAwaiter();
+      public TaskAwaiter<SingleUseSocketAsyncEventArgs>
+      GetAwaiter() => this.completion.Task.GetAwaiter();
 
-        public void Complete() => this.completion.TrySetResult(this);
+      public void Complete() => this.completion.TrySetResult(this);
 
-        public void OnCompleted(Action continuation) => this.GetAwaiter().OnCompleted(continuation);
+      public void OnCompleted(Action continuation) =>
+          this.GetAwaiter().OnCompleted(continuation);
 
-        public void UnsafeOnCompleted(Action continuation) => this.GetAwaiter().UnsafeOnCompleted(continuation);
+      public void UnsafeOnCompleted(Action continuation) =>
+          this.GetAwaiter().UnsafeOnCompleted(continuation);
 
-        protected override void OnCompleted(SocketAsyncEventArgs _) => this.completion.TrySetResult(this);
+      protected override void
+      OnCompleted(SocketAsyncEventArgs _) => this.completion.TrySetResult(this);
     }
-}
+  }
 
-[Serializable]
-public sealed class SocketConnectionException : OrleansException
-{
-    public SocketConnectionException(string message) : base(message) { }
+  [Serializable]
+  public sealed class SocketConnectionException : OrleansException {
+    public SocketConnectionException(string message) : base(message) {}
 
-    public SocketConnectionException(string message, Exception innerException) : base(message, innerException) { }
+    public SocketConnectionException(string message, Exception innerException)
+        : base(message, innerException) {}
 
-    public SocketConnectionException(SerializationInfo info, StreamingContext context)
-        : base(info, context)
-    {
-    }
-}
+    public SocketConnectionException(SerializationInfo info,
+                                     StreamingContext context)
+        : base(info, context) {}
+  }
 }
