9374eb05f1b14eb11e2825122225d27923351c67
Restyled.io <commits@restyled.io>
2019-12-16 22:03:34 +0000

    Restyled by shfmt

--- run-build.sh
+++ run-build.sh
@@ -8,39 +8,39 @@ set -e
 
 SOURCE="${BASH_SOURCE[0]}"
 while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
-  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
   SOURCE="$(readlink "$SOURCE")"
   [[ "$SOURCE" != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
 done
-DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+DIR="$(cd -P "$(dirname "$SOURCE")" && pwd)"
 REPOROOT="$DIR"
 
 # Set nuget package cache under the repo
 [ -z "$NUGET_PACKAGES" ] && export NUGET_PACKAGES="$REPOROOT/.nuget/packages"
 
-args=( "$@" )
+args=("$@")
 
 while [[ $# > 0 ]]; do
-    lowerI="$(echo "$1" | awk '{print tolower($0)}')"
-    case $lowerI in
-        -c|--configuration)
-            export CONFIGURATION="$2"
-            args=( "${args[@]/$1}" )
-            args=( "${args[@]/$2}" )
-            shift
-            ;;
-        # This is here just to eat away this parameter because CI still passes this in.
-        --targets)
-            args=( "${args[@]/$1}" )
-            args=( "${args[@]/$2}" )
-            shift
-            ;;
-        *)
-            break
-            ;;
-    esac
+  lowerI="$(echo "$1" | awk '{print tolower($0)}')"
+  case $lowerI in
+    -c | --configuration)
+      export CONFIGURATION="$2"
+      args=("${args[@]/$1/}")
+      args=("${args[@]/$2/}")
+      shift
+      ;;
+    # This is here just to eat away this parameter because CI still passes this in.
+    --targets)
+      args=("${args[@]/$1/}")
+      args=("${args[@]/$2/}")
+      shift
+      ;;
+    *)
+      break
+      ;;
+  esac
 
-    shift
+  shift
 done
 
 # $args array may have empty elements in it.
@@ -51,11 +51,10 @@ temp="${args[@]}"
 args=("$temp")
 
 # Increases the file descriptors limit for this bash. It prevents an issue we were hitting during restore
-FILE_DESCRIPTOR_LIMIT=$( ulimit -n )
-if [ "$FILE_DESCRIPTOR_LIMIT" -lt 1024 ]
-then
-    echo "Increasing file description limit to 1024"
-    ulimit -n 1024
+FILE_DESCRIPTOR_LIMIT=$(ulimit -n)
+if [ "$FILE_DESCRIPTOR_LIMIT" -lt 1024 ]; then
+  echo "Increasing file description limit to 1024"
+  ulimit -n 1024
 fi
 
 dotnet build Orleans.sln /p:Configuration="$CONFIGURATION" "${args[@]}"
