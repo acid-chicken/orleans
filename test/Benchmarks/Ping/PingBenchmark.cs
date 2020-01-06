6f4bf05b6974ec737f65c484b41f3c0fd0010142
Restyled.io <commits@restyled.io>
2020-01-06 18:03:36 +0000

    Restyled by astyle

--- test/Benchmarks/Ping/PingBenchmark.cs
+++ test/Benchmarks/Ping/PingBenchmark.cs
@@ -13,138 +13,138 @@ using Orleans.Hosting;
 
 namespace Benchmarks.Ping
 {
-    [MemoryDiagnoser]
-    public class PingBenchmark : IDisposable 
+[MemoryDiagnoser]
+public class PingBenchmark : IDisposable
+{
+    private readonly List<ISiloHost> hosts = new List<ISiloHost>();
+    private readonly IPingGrain grain;
+    private readonly IClusterClient client;
+
+    public PingBenchmark() : this(1, true) { }
+
+    public PingBenchmark(int numSilos, bool startClient, bool grainsOnSecondariesOnly = false)
     {
-        private readonly List<ISiloHost> hosts = new List<ISiloHost>();
-        private readonly IPingGrain grain;
-        private readonly IClusterClient client;
-
-        public PingBenchmark() : this(1, true) { }
-
-        public PingBenchmark(int numSilos, bool startClient, bool grainsOnSecondariesOnly = false)
+        for (var i = 0; i < numSilos; ++i)
         {
-            for (var i = 0; i < numSilos; ++i)
-            {
-                var primary = i == 0 ? null : new IPEndPoint(IPAddress.Loopback, 11111);
-                var siloBuilder = new SiloHostBuilder()
-                    .ConfigureDefaults()
-                    .UseLocalhostClustering(
-                        siloPort: 11111 + i,
-                        gatewayPort: 30000 + i,
-                        primarySiloEndpoint: primary);
+            var primary = i == 0 ? null : new IPEndPoint(IPAddress.Loopback, 11111);
+            var siloBuilder = new SiloHostBuilder()
+            .ConfigureDefaults()
+            .UseLocalhostClustering(
+                siloPort: 11111 + i,
+                gatewayPort: 30000 + i,
+                primarySiloEndpoint: primary);
 
-                if (i == 0 && grainsOnSecondariesOnly)
+            if (i == 0 && grainsOnSecondariesOnly)
+            {
+                siloBuilder.ConfigureApplicationParts(parts =>
+                                                      parts.AddApplicationPart(typeof(IPingGrain).Assembly));
+                siloBuilder.ConfigureServices(services =>
                 {
-                    siloBuilder.ConfigureApplicationParts(parts =>
-                        parts.AddApplicationPart(typeof(IPingGrain).Assembly));
-                    siloBuilder.ConfigureServices(services =>
-                    {
-                        services.Remove(services.First(s => s.ImplementationType?.Name == "ApplicationPartValidator"));
-                    });
-                }
-                else
-                {
-                    siloBuilder.ConfigureApplicationParts(parts =>
-                        parts.AddApplicationPart(typeof(IPingGrain).Assembly)
-                             .AddApplicationPart(typeof(PingGrain).Assembly));
-                }
-
-                var silo = siloBuilder.Build();
-                silo.StartAsync().GetAwaiter().GetResult();
-                this.hosts.Add(silo);
+                    services.Remove(services.First(s => s.ImplementationType?.Name == "ApplicationPartValidator"));
+                });
             }
-
-            if (grainsOnSecondariesOnly) Thread.Sleep(4000);
-
-            if (startClient)
+            else
             {
-                var clientBuilder = new ClientBuilder()
-                    .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IPingGrain).Assembly))
-                    .Configure<ClusterOptions>(options => options.ClusterId = options.ServiceId = "dev");
-
-                if (numSilos == 1)
-                {
-                    clientBuilder.UseLocalhostClustering();
-                }
-                else
-                {
-                    var gateways = Enumerable.Range(30000, numSilos).Select(i => new IPEndPoint(IPAddress.Loopback, i)).ToArray();
-                    clientBuilder.UseStaticClustering(gateways);
-                }
-
-                this.client = clientBuilder.Build();
-                this.client.Connect().GetAwaiter().GetResult();
-                var grainFactory = this.client;
-
-                this.grain = grainFactory.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode());
-                this.grain.Run().GetAwaiter().GetResult();
+                siloBuilder.ConfigureApplicationParts(parts =>
+                                                      parts.AddApplicationPart(typeof(IPingGrain).Assembly)
+                                                      .AddApplicationPart(typeof(PingGrain).Assembly));
             }
-        }
-        
-        [Benchmark]
-        public Task Ping() => grain.Run();
 
-        public async Task PingForever()
-        {
-            while (true)
-            {
-                await grain.Run();
-            }
+            var silo = siloBuilder.Build();
+            silo.StartAsync().GetAwaiter().GetResult();
+            this.hosts.Add(silo);
         }
 
-        public Task PingConcurrent() => this.Run(
-            runs: 3,
-            grainFactory: this.client,
-            blocksPerWorker: 10);
+        if (grainsOnSecondariesOnly) Thread.Sleep(4000);
 
-        public Task PingConcurrentHostedClient(int blocksPerWorker = 30) => this.Run(
-            runs: 3,
-            grainFactory: (IGrainFactory)this.hosts[0].Services.GetService(typeof(IGrainFactory)),
-            blocksPerWorker: blocksPerWorker);
-
-        private async Task Run(int runs, IGrainFactory grainFactory, int blocksPerWorker)
+        if (startClient)
         {
-            var loadGenerator = new ConcurrentLoadGenerator<IPingGrain>(
-                maxConcurrency: 250,
-                blocksPerWorker: blocksPerWorker,
-                requestsPerBlock: 500,
-                issueRequest: g => g.Run(),
-                getStateForWorker: workerId => grainFactory.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode()));
-            await loadGenerator.Warmup();
-            while (runs-- > 0) await loadGenerator.Run();
-        }
+            var clientBuilder = new ClientBuilder()
+            .ConfigureApplicationParts(parts => parts.AddApplicationPart(typeof(IPingGrain).Assembly))
+            .Configure<ClusterOptions>(options => options.ClusterId = options.ServiceId = "dev");
 
-        public async Task PingPongForever()
-        {
-            var other = this.client.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode());
-            while (true)
+            if (numSilos == 1)
             {
-                await grain.PingPongInterleave(other, 100);
+                clientBuilder.UseLocalhostClustering();
             }
-        }
-
-        public async Task Shutdown()
-        {
-            if (this.client is IClusterClient c)
+            else
             {
-                await c.Close();
-                c.Dispose();
+                var gateways = Enumerable.Range(30000, numSilos).Select(i => new IPEndPoint(IPAddress.Loopback, i)).ToArray();
+                clientBuilder.UseStaticClustering(gateways);
             }
 
-            this.hosts.Reverse();
-            foreach (var h in this.hosts)
-            {
-                await h.StopAsync();
-                h.Dispose();
-            }
-        }
+            this.client = clientBuilder.Build();
+            this.client.Connect().GetAwaiter().GetResult();
+            var grainFactory = this.client;
 
-        [GlobalCleanup]
-        public void Dispose()
-        {
-            (this.client as IDisposable)?.Dispose(); 
-            this.hosts.ForEach(h => h.Dispose());
+            this.grain = grainFactory.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode());
+            this.grain.Run().GetAwaiter().GetResult();
         }
     }
+
+    [Benchmark]
+    public Task Ping() => grain.Run();
+
+    public async Task PingForever()
+    {
+        while (true)
+        {
+            await grain.Run();
+        }
+    }
+
+    public Task PingConcurrent() => this.Run(
+        runs: 3,
+        grainFactory: this.client,
+        blocksPerWorker: 10);
+
+    public Task PingConcurrentHostedClient(int blocksPerWorker = 30) => this.Run(
+        runs: 3,
+        grainFactory: (IGrainFactory)this.hosts[0].Services.GetService(typeof(IGrainFactory)),
+        blocksPerWorker: blocksPerWorker);
+
+    private async Task Run(int runs, IGrainFactory grainFactory, int blocksPerWorker)
+    {
+        var loadGenerator = new ConcurrentLoadGenerator<IPingGrain>(
+            maxConcurrency: 250,
+            blocksPerWorker: blocksPerWorker,
+            requestsPerBlock: 500,
+            issueRequest: g => g.Run(),
+            getStateForWorker: workerId => grainFactory.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode()));
+        await loadGenerator.Warmup();
+        while (runs-- > 0) await loadGenerator.Run();
+    }
+
+    public async Task PingPongForever()
+    {
+        var other = this.client.GetGrain<IPingGrain>(Guid.NewGuid().GetHashCode());
+        while (true)
+        {
+            await grain.PingPongInterleave(other, 100);
+        }
+    }
+
+    public async Task Shutdown()
+    {
+        if (this.client is IClusterClient c)
+        {
+            await c.Close();
+            c.Dispose();
+        }
+
+        this.hosts.Reverse();
+        foreach (var h in this.hosts)
+        {
+            await h.StopAsync();
+            h.Dispose();
+        }
+    }
+
+    [GlobalCleanup]
+    public void Dispose()
+    {
+        (this.client as IDisposable)?.Dispose();
+        this.hosts.ForEach(h => h.Dispose());
+    }
+}
 }
--- test/Benchmarks/Program.cs
+++ test/Benchmarks/Program.cs
@@ -14,230 +14,230 @@ using BenchmarkDotNet.Toolchains.CsProj;
 
 namespace Benchmarks
 {
-    class Program
+class Program
+{
+    private static readonly Dictionary<string, Action> _benchmarks = new Dictionary<string, Action>
     {
-        private static readonly Dictionary<string, Action> _benchmarks = new Dictionary<string, Action>
+        ["MapReduce"] = () =>
         {
-            ["MapReduce"] = () =>
-            {
-                RunBenchmark(
-                "Running MapReduce benchmark", 
+            RunBenchmark(
+                "Running MapReduce benchmark",
                 () =>
-                {
-                    var mapReduceBenchmark = new MapReduceBenchmark();
-                    mapReduceBenchmark.BenchmarkSetup();
-                    return mapReduceBenchmark;
-                },
-                benchmark => benchmark.Bench().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["Serialization"] = () =>
             {
-                BenchmarkRunner.Run<SerializationBenchmarks>();
+                var mapReduceBenchmark = new MapReduceBenchmark();
+                mapReduceBenchmark.BenchmarkSetup();
+                return mapReduceBenchmark;
             },
-            ["Transactions.Memory"] = () =>
-            {
-                RunBenchmark(
+            benchmark => benchmark.Bench().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["Serialization"] = () =>
+        {
+            BenchmarkRunner.Run<SerializationBenchmarks>();
+        },
+        ["Transactions.Memory"] = () =>
+        {
+            RunBenchmark(
                 "Running Transactions benchmark",
                 () =>
-                {
-                    var benchmark = new TransactionBenchmark(2, 20000, 5000);
-                    benchmark.MemorySetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["Transactions.Memory.Throttled"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new TransactionBenchmark(2, 20000, 5000);
+                benchmark.MemorySetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["Transactions.Memory.Throttled"] = () =>
+        {
+            RunBenchmark(
                 "Running Transactions benchmark",
                 () =>
-                {
-                    var benchmark = new TransactionBenchmark(2, 200000, 15000);
-                    benchmark.MemoryThrottledSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["Transactions.Azure"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new TransactionBenchmark(2, 200000, 15000);
+                benchmark.MemoryThrottledSetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["Transactions.Azure"] = () =>
+        {
+            RunBenchmark(
                 "Running Transactions benchmark",
                 () =>
-                {
-                    var benchmark = new TransactionBenchmark(2, 20000, 5000);
-                    benchmark.AzureSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["Transactions.Azure.Throttled"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new TransactionBenchmark(2, 20000, 5000);
+                benchmark.AzureSetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["Transactions.Azure.Throttled"] = () =>
+        {
+            RunBenchmark(
                 "Running Transactions benchmark",
                 () =>
-                {
-                    var benchmark = new TransactionBenchmark(2, 200000, 15000);
-                    benchmark.AzureThrottledSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["Transactions.Azure.Overloaded"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new TransactionBenchmark(2, 200000, 15000);
+                benchmark.AzureThrottledSetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["Transactions.Azure.Overloaded"] = () =>
+        {
+            RunBenchmark(
                 "Running Transactions benchmark",
                 () =>
-                {
-                    var benchmark = new TransactionBenchmark(2, 200000, 15000);
-                    benchmark.AzureSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["SequentialPing"] = () =>
             {
-                BenchmarkRunner.Run<PingBenchmark>();
+                var benchmark = new TransactionBenchmark(2, 200000, 15000);
+                benchmark.AzureSetup();
+                return benchmark;
             },
-            ["ConcurrentPing"] = () =>
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["SequentialPing"] = () =>
+        {
+            BenchmarkRunner.Run<PingBenchmark>();
+        },
+        ["ConcurrentPing"] = () =>
+        {
             {
-                {
-                    Console.WriteLine("## Client to Silo ##");
-                    var test = new PingBenchmark(numSilos: 1, startClient: true);
-                    test.PingConcurrent().GetAwaiter().GetResult();
-                    test.Shutdown().GetAwaiter().GetResult();
-                }
-                GC.Collect();
-                {
-                    Console.WriteLine("## Client to 2 Silos ##");
-                    var test = new PingBenchmark(numSilos: 2, startClient: true);
-                    test.PingConcurrent().GetAwaiter().GetResult();
-                    test.Shutdown().GetAwaiter().GetResult();
-                }
-                GC.Collect();
-                {
-                    Console.WriteLine("## Hosted Client ##");
-                    var test = new PingBenchmark(numSilos: 1, startClient: false);
-                    test.PingConcurrentHostedClient().GetAwaiter().GetResult();
-                    test.Shutdown().GetAwaiter().GetResult();
-                }
-                GC.Collect();
-                {
-                    // All calls are cross-silo because the calling silo doesn't have any grain classes.
-                    Console.WriteLine("## Silo to Silo ##");
-                    var test = new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true);
-                    test.PingConcurrentHostedClient(blocksPerWorker: 10).GetAwaiter().GetResult();
-                    test.Shutdown().GetAwaiter().GetResult();
-                }
-            },
-            ["ConcurrentPing_OneSilo"] = () =>
+                Console.WriteLine("## Client to Silo ##");
+                var test = new PingBenchmark(numSilos: 1, startClient: true);
+                test.PingConcurrent().GetAwaiter().GetResult();
+                test.Shutdown().GetAwaiter().GetResult();
+            }
+            GC.Collect();
             {
-                new PingBenchmark(numSilos: 1, startClient: true).PingConcurrent().GetAwaiter().GetResult();
-            },
-            ["ConcurrentPing_TwoSilos"] = () =>
+                Console.WriteLine("## Client to 2 Silos ##");
+                var test = new PingBenchmark(numSilos: 2, startClient: true);
+                test.PingConcurrent().GetAwaiter().GetResult();
+                test.Shutdown().GetAwaiter().GetResult();
+            }
+            GC.Collect();
             {
-                new PingBenchmark(numSilos: 2, startClient: true).PingConcurrent().GetAwaiter().GetResult();
-            },
-            ["ConcurrentPing_HostedClient"] = () =>
+                Console.WriteLine("## Hosted Client ##");
+                var test = new PingBenchmark(numSilos: 1, startClient: false);
+                test.PingConcurrentHostedClient().GetAwaiter().GetResult();
+                test.Shutdown().GetAwaiter().GetResult();
+            }
+            GC.Collect();
             {
-                new PingBenchmark(numSilos: 1, startClient: false).PingConcurrentHostedClient().GetAwaiter().GetResult();
-            },
-            ["ConcurrentPing_SiloToSilo"] = () =>
-            {
-                new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true).PingConcurrentHostedClient(blocksPerWorker: 10).GetAwaiter().GetResult();                
-            },
-            ["ConcurrentPing_SiloToSilo_Long"] = () =>
-            {
-                new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true).PingConcurrentHostedClient(blocksPerWorker: 1000).GetAwaiter().GetResult();
-            },
-            ["PingForever"] = () =>
-            {
-                new PingBenchmark().PingForever().GetAwaiter().GetResult();
-            },
-            ["PingPongForever"] = () =>
-            {
-                new PingBenchmark().PingPongForever().GetAwaiter().GetResult();
-            },
-            ["GrainStorage.Memory"] = () =>
-            {
-                RunBenchmark(
+                // All calls are cross-silo because the calling silo doesn't have any grain classes.
+                Console.WriteLine("## Silo to Silo ##");
+                var test = new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true);
+                test.PingConcurrentHostedClient(blocksPerWorker: 10).GetAwaiter().GetResult();
+                test.Shutdown().GetAwaiter().GetResult();
+            }
+        },
+        ["ConcurrentPing_OneSilo"] = () =>
+        {
+            new PingBenchmark(numSilos: 1, startClient: true).PingConcurrent().GetAwaiter().GetResult();
+        },
+        ["ConcurrentPing_TwoSilos"] = () =>
+        {
+            new PingBenchmark(numSilos: 2, startClient: true).PingConcurrent().GetAwaiter().GetResult();
+        },
+        ["ConcurrentPing_HostedClient"] = () =>
+        {
+            new PingBenchmark(numSilos: 1, startClient: false).PingConcurrentHostedClient().GetAwaiter().GetResult();
+        },
+        ["ConcurrentPing_SiloToSilo"] = () =>
+        {
+            new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true).PingConcurrentHostedClient(blocksPerWorker: 10).GetAwaiter().GetResult();
+        },
+        ["ConcurrentPing_SiloToSilo_Long"] = () =>
+        {
+            new PingBenchmark(numSilos: 2, startClient: false, grainsOnSecondariesOnly: true).PingConcurrentHostedClient(blocksPerWorker: 1000).GetAwaiter().GetResult();
+        },
+        ["PingForever"] = () =>
+        {
+            new PingBenchmark().PingForever().GetAwaiter().GetResult();
+        },
+        ["PingPongForever"] = () =>
+        {
+            new PingBenchmark().PingPongForever().GetAwaiter().GetResult();
+        },
+        ["GrainStorage.Memory"] = () =>
+        {
+            RunBenchmark(
                 "Running grain storage benchmark against memory",
                 () =>
-                {
-                    var benchmark = new GrainStorageBenchmark();
-                    benchmark.MemorySetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["GrainStorage.AzureTable"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new GrainStorageBenchmark();
+                benchmark.MemorySetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["GrainStorage.AzureTable"] = () =>
+        {
+            RunBenchmark(
                 "Running grain storage benchmark against Azure Table",
                 () =>
-                {
-                    var benchmark = new GrainStorageBenchmark();
-                    benchmark.AzureTableSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
-            },
-            ["GrainStorage.AzureBlob"] = () =>
             {
-                RunBenchmark(
+                var benchmark = new GrainStorageBenchmark();
+                benchmark.AzureTableSetup();
+                return benchmark;
+            },
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+        ["GrainStorage.AzureBlob"] = () =>
+        {
+            RunBenchmark(
                 "Running grain storage benchmark against Azure Blob",
                 () =>
-                {
-                    var benchmark = new GrainStorageBenchmark();
-                    benchmark.AzureBlobSetup();
-                    return benchmark;
-                },
-                benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
-                benchmark => benchmark.Teardown());
+            {
+                var benchmark = new GrainStorageBenchmark();
+                benchmark.AzureBlobSetup();
+                return benchmark;
             },
-        };
+            benchmark => benchmark.RunAsync().GetAwaiter().GetResult(),
+            benchmark => benchmark.Teardown());
+        },
+    };
 
-        // requires benchmark name or 'All' word as first parameter
-        public static void Main(string[] args)
+    // requires benchmark name or 'All' word as first parameter
+    public static void Main(string[] args)
+    {
+        if (args.Length > 0 && args[0].Equals("all", StringComparison.InvariantCultureIgnoreCase))
         {
-            if (args.Length > 0 && args[0].Equals("all", StringComparison.InvariantCultureIgnoreCase))
-            {
-                Console.WriteLine("Running full benchmarks suite");
-                _benchmarks.Select(pair => pair.Value).ToList().ForEach(action => action());
-                return;
-            }
-
-            if (args.Length == 0 || !_benchmarks.ContainsKey(args[0]))
-            {
-                Console.WriteLine("Please, select benchmark, list of available:");
-                _benchmarks
-                    .Select(pair => pair.Key)
-                    .ToList()
-                    .ForEach(Console.WriteLine);
-                Console.WriteLine("All");
-                return;
-            }
-
-            _benchmarks[args[0]]();
+            Console.WriteLine("Running full benchmarks suite");
+            _benchmarks.Select(pair => pair.Value).ToList().ForEach(action => action());
+            return;
         }
 
-        private static void RunBenchmark<T>(string name, Func<T> init, Action<T> benchmarkAction, Action<T> tearDown)
+        if (args.Length == 0 || !_benchmarks.ContainsKey(args[0]))
         {
-            Console.WriteLine(name);
-            var bench = init();
-            var stopWatch = Stopwatch.StartNew();
-            benchmarkAction(bench);
-            Console.WriteLine($"Elapsed milliseconds: {stopWatch.ElapsedMilliseconds}");
-            Console.WriteLine("Press any key to continue ...");
-            tearDown(bench);
-            Console.ReadLine();
+            Console.WriteLine("Please, select benchmark, list of available:");
+            _benchmarks
+            .Select(pair => pair.Key)
+            .ToList()
+            .ForEach(Console.WriteLine);
+            Console.WriteLine("All");
+            return;
         }
+
+        _benchmarks[args[0]]();
     }
+
+    private static void RunBenchmark<T>(string name, Func<T> init, Action<T> benchmarkAction, Action<T> tearDown)
+    {
+        Console.WriteLine(name);
+        var bench = init();
+        var stopWatch = Stopwatch.StartNew();
+        benchmarkAction(bench);
+        Console.WriteLine($"Elapsed milliseconds: {stopWatch.ElapsedMilliseconds}");
+        Console.WriteLine("Press any key to continue ...");
+        tearDown(bench);
+        Console.ReadLine();
+    }
+}
 }
