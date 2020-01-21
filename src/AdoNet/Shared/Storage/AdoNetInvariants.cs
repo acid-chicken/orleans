50e2c6855aadd66d316ea9a47ec9f12a7adcb05b
Restyled.io <commits@restyled.io>
2020-01-21 07:03:58 +0000

    Restyled by astyle

--- src/AdoNet/Shared/Storage/AdoNetInvariants.cs
+++ src/AdoNet/Shared/Storage/AdoNetInvariants.cs
@@ -13,51 +13,53 @@ namespace Orleans.Tests.SqlUtils
 // No default namespace intentionally to cause compile errors if something is not defined
 #endif
 {
+/// <summary>
+/// A holder for well known, vendor specific connector class invariant names.
+/// </summary>
+internal static class AdoNetInvariants
+{
     /// <summary>
-    /// A holder for well known, vendor specific connector class invariant names.
+    /// A list of the supported invariants.
     /// </summary>
-    internal static class AdoNetInvariants
+    public static ICollection<string> Invariants {
+        get;
+    } = new Collection<string>(new List<string>(new[]
     {
-        /// <summary>
-        /// A list of the supported invariants.
-        /// </summary>
-        public static ICollection<string> Invariants { get; } = new Collection<string>(new List<string>(new[]
-        {
-            InvariantNameMySql,
-            InvariantNameOracleDatabase,
-            InvariantNamePostgreSql,
-            InvariantNameSqlLite,
-            InvariantNameSqlServer
-        }));
+        InvariantNameMySql,
+        InvariantNameOracleDatabase,
+        InvariantNamePostgreSql,
+        InvariantNameSqlLite,
+        InvariantNameSqlServer
+    }));
 
-        /// <summary>
-        /// Microsoft SQL Server invariant name string.
-        /// </summary>
-        public const string InvariantNameSqlServer = "System.Data.SqlClient";
+    /// <summary>
+    /// Microsoft SQL Server invariant name string.
+    /// </summary>
+    public const string InvariantNameSqlServer = "System.Data.SqlClient";
 
-        /// <summary>
-        /// Oracle Database server invariant name string.
-        /// </summary>
-        public const string InvariantNameOracleDatabase = "Oracle.DataAccess.Client";
+    /// <summary>
+    /// Oracle Database server invariant name string.
+    /// </summary>
+    public const string InvariantNameOracleDatabase = "Oracle.DataAccess.Client";
 
-        /// <summary>
-        /// SQLite invariant name string.
-        /// </summary>
-        public const string InvariantNameSqlLite = "System.Data.SQLite";
+    /// <summary>
+    /// SQLite invariant name string.
+    /// </summary>
+    public const string InvariantNameSqlLite = "System.Data.SQLite";
 
-        /// <summary>
-        /// MySql invariant name string.
-        /// </summary>
-        public const string InvariantNameMySql = "MySql.Data.MySqlClient";
+    /// <summary>
+    /// MySql invariant name string.
+    /// </summary>
+    public const string InvariantNameMySql = "MySql.Data.MySqlClient";
 
-        /// <summary>
-        /// PostgreSQL invariant name string.
-        /// </summary>
-        public const string InvariantNamePostgreSql = "Npgsql";
+    /// <summary>
+    /// PostgreSQL invariant name string.
+    /// </summary>
+    public const string InvariantNamePostgreSql = "Npgsql";
 
-        /// <summary>
-        /// Dotnet core Microsoft SQL Server invariant name string.
-        /// </summary>
-        public const string InvariantNameSqlServerDotnetCore = "Microsoft.Data.SqlClient";
-    }
+    /// <summary>
+    /// Dotnet core Microsoft SQL Server invariant name string.
+    /// </summary>
+    public const string InvariantNameSqlServerDotnetCore = "Microsoft.Data.SqlClient";
+}
 }
