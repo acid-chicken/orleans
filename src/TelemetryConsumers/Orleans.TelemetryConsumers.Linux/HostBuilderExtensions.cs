7fe5689cc3bbc4f24800a1e501ef8bbcafd63aa6
Restyled.io <commits@restyled.io>
2020-02-05 16:49:46 +0000

    Restyled by clang-format

--- src/TelemetryConsumers/Orleans.TelemetryConsumers.Linux/HostBuilderExtensions.cs
+++ src/TelemetryConsumers/Orleans.TelemetryConsumers.Linux/HostBuilderExtensions.cs
@@ -9,91 +9,91 @@ using Orleans.Configuration.Internal;
 using Orleans.Hosting;
 using Orleans.Runtime;
 
-namespace Orleans.Statistics
-{
-/// <summary>
-/// Validates <see cref="LinuxEnvironmentStatistics"/> requirements for.
-/// </summary>
-internal class LinuxEnvironmentStatisticsValidator : IConfigurationValidator
-{
-    internal static readonly string InvalidOS = $"Tried to add '{nameof(LinuxEnvironmentStatistics)}' on non-linux OS";
+namespace Orleans.Statistics {
+  /// <summary>
+  /// Validates <see cref="LinuxEnvironmentStatistics"/> requirements for.
+  /// </summary>
+  internal class LinuxEnvironmentStatisticsValidator : IConfigurationValidator {
+    internal static readonly string InvalidOS =
+        $"Tried to add '{nameof(LinuxEnvironmentStatistics)}' on non-linux OS";
     private readonly ILogger _logger;
 
-    public LinuxEnvironmentStatisticsValidator(ILogger<LinuxEnvironmentStatisticsValidator> logger)
-    {
-        _logger = logger;
+    public LinuxEnvironmentStatisticsValidator(
+        ILogger<LinuxEnvironmentStatisticsValidator>logger) {
+      _logger = logger;
     }
 
     /// <inheritdoc />
-    public void ValidateConfiguration()
-    {
-        var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
+    public void ValidateConfiguration() {
+      var isLinux = RuntimeInformation.IsOSPlatform(OSPlatform.Linux);
 
-        if (!isLinux)
-        {
-            _logger.LogWarning((int)ErrorCode.OS_InvalidOS, InvalidOS);
-        }
+      if (!isLinux) {
+        _logger.LogWarning((int) ErrorCode.OS_InvalidOS, InvalidOS);
+      }
 
-        var missingFiles = LinuxEnvironmentStatistics.RequiredFiles
-        .Select(f => new {
-            FilePath = f, FileExists = File.Exists(f)
-        })
-        .Where(f => !f.FileExists)
-        .ToList();
+      var missingFiles =
+          LinuxEnvironmentStatistics.RequiredFiles
+              .Select(f => new {FilePath = f, FileExists = File.Exists(f)})
+              .Where(f => !f.FileExists)
+              .ToList();
 
-        if (missingFiles.Any())
-        {
-            var paths = string.Join(", ", missingFiles.Select(f => f.FilePath));
-            throw new OrleansConfigurationException($"Missing files for {nameof(LinuxEnvironmentStatistics)}: {paths}");
-        }
+      if (missingFiles.Any()) {
+        var paths = string.Join(", ", missingFiles.Select(f => f.FilePath));
+        throw new OrleansConfigurationException(
+            $"Missing files for {nameof(LinuxEnvironmentStatistics)}: {paths}");
+      }
     }
-}
+  }
 
-public static class SiloHostBuilderExtensions
-{
+  public static class SiloHostBuilderExtensions {
     /// <summary>
     /// Use Linux host environment statistics
     /// </summary>
-    public static ISiloHostBuilder UseLinuxEnvironmentStatistics(this ISiloHostBuilder builder)
-    {
-        return builder.ConfigureServices(services =>
-        {
-            services.AddTransient<IConfigurationValidator, LinuxEnvironmentStatisticsValidator>();
-            services.AddSingleton<LinuxEnvironmentStatistics>();
-            services.AddFromExisting<IHostEnvironmentStatistics, LinuxEnvironmentStatistics>();
-            services.AddFromExisting<ILifecycleParticipant<ISiloLifecycle>, LinuxEnvironmentStatistics>();
-        });
+    public static ISiloHostBuilder
+    UseLinuxEnvironmentStatistics(this ISiloHostBuilder builder) {
+      return builder.ConfigureServices(services => {
+        services.AddTransient<IConfigurationValidator,
+                              LinuxEnvironmentStatisticsValidator>();
+        services.AddSingleton<LinuxEnvironmentStatistics>();
+        services.AddFromExisting<IHostEnvironmentStatistics,
+                                 LinuxEnvironmentStatistics>();
+        services.AddFromExisting<ILifecycleParticipant<ISiloLifecycle>,
+                                 LinuxEnvironmentStatistics>();
+      });
     }
 
     /// <summary>
     /// Use Linux host environment statistics
     /// </summary>
-    public static ISiloBuilder UseLinuxEnvironmentStatistics(this ISiloBuilder builder)
-    {
-        return builder.ConfigureServices(services =>
-        {
-            services.AddTransient<IConfigurationValidator, LinuxEnvironmentStatisticsValidator>();
-            services.AddSingleton<LinuxEnvironmentStatistics>();
-            services.AddFromExisting<IHostEnvironmentStatistics, LinuxEnvironmentStatistics>();
-            services.AddFromExisting<ILifecycleParticipant<ISiloLifecycle>, LinuxEnvironmentStatistics>();
-        });
+    public static ISiloBuilder
+    UseLinuxEnvironmentStatistics(this ISiloBuilder builder) {
+      return builder.ConfigureServices(services => {
+        services.AddTransient<IConfigurationValidator,
+                              LinuxEnvironmentStatisticsValidator>();
+        services.AddSingleton<LinuxEnvironmentStatistics>();
+        services.AddFromExisting<IHostEnvironmentStatistics,
+                                 LinuxEnvironmentStatistics>();
+        services.AddFromExisting<ILifecycleParticipant<ISiloLifecycle>,
+                                 LinuxEnvironmentStatistics>();
+      });
     }
-}
+  }
 
-public static class ClientBuilderExtensions
-{
+  public static class ClientBuilderExtensions {
     /// <summary>
     /// Use Linux host environment statistics
     /// </summary>
-    public static IClientBuilder UseLinuxEnvironmentStatistics(this IClientBuilder builder)
-    {
-        return builder.ConfigureServices(services =>
-        {
-            services.AddTransient<IConfigurationValidator, LinuxEnvironmentStatisticsValidator>();
-            services.AddSingleton<LinuxEnvironmentStatistics>();
-            services.AddFromExisting<IHostEnvironmentStatistics, LinuxEnvironmentStatistics>();
-            services.AddFromExisting<ILifecycleParticipant<IClusterClientLifecycle>, LinuxEnvironmentStatistics>();
-        });
+    public static IClientBuilder
+    UseLinuxEnvironmentStatistics(this IClientBuilder builder) {
+      return builder.ConfigureServices(services => {
+        services.AddTransient<IConfigurationValidator,
+                              LinuxEnvironmentStatisticsValidator>();
+        services.AddSingleton<LinuxEnvironmentStatistics>();
+        services.AddFromExisting<IHostEnvironmentStatistics,
+                                 LinuxEnvironmentStatistics>();
+        services.AddFromExisting<ILifecycleParticipant<IClusterClientLifecycle>,
+                                 LinuxEnvironmentStatistics>();
+      });
     }
-}
+  }
 }
