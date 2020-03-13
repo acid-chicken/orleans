5b6bdf0426e876dfa98b3acc61f4950fc66956de
Restyled.io <commits@restyled.io>
2020-03-13 06:31:15 +0000

    Restyled by astyle

--- src/Orleans.CodeGenerator/Compatibility/RoslynTypeHelper.cs
+++ src/Orleans.CodeGenerator/Compatibility/RoslynTypeHelper.cs
@@ -5,13 +5,13 @@ using Microsoft.CodeAnalysis;
 
 namespace Orleans.CodeGenerator.Compatibility
 {
-    internal static class RoslynTypeHelper
+internal static class RoslynTypeHelper
+{
+    public static bool IsSystemNamespace(INamespaceSymbol ns)
     {
-        public static bool IsSystemNamespace(INamespaceSymbol ns)
-        {
-            if (ns is null || ns.IsGlobalNamespace) return false;
-            if (ns.ContainingNamespace is INamespaceSymbol parent && !parent.IsGlobalNamespace) return IsSystemNamespace(parent);
-            return string.Equals(ns.Name, "System", StringComparison.Ordinal) || ns.Name.StartsWith("System.", StringComparison.Ordinal);
-        }
+        if (ns is null || ns.IsGlobalNamespace) return false;
+        if (ns.ContainingNamespace is INamespaceSymbol parent && !parent.IsGlobalNamespace) return IsSystemNamespace(parent);
+        return string.Equals(ns.Name, "System", StringComparison.Ordinal) || ns.Name.StartsWith("System.", StringComparison.Ordinal);
     }
+}
 }
